3ffaebaa41b6cc2f41ebd2837d63b5faff7d0a70
AviionOG <78471490+AviionOG@users.noreply.github.com>
2022-08-04 23:15:56 -0500

    Delete Başlat.bat

--- Başlat.bat
+++
@@ -1,3 +0,0 @@
-cd /d C:\Users\Administrator\Desktop\kibra-v3-lite\Server-Data
-start C:\Users\Administrator\Desktop\kibra-v3-lite\ARTIFACTS\FXServer.exe +exec  C:\Users\Administrator\Desktop\kibra-v3-lite\Server-Data\server.cfg +set sv_enforceGameBuild 2189 +set svgui_disable true +set onesync on +set onesync_enableInfinity 1
-exit
